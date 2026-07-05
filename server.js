5bc7ea09a696e29696e2432d2dc118149a1147db
Jafari-mussa <jafarimfara@gmail.com>
2026-07-05 14:51:03 +0300

    fix

--- server.js
+++ server.js
@@ -6,10 +6,14 @@ const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
 
+const path = require("path");
+
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+
+app.use(express.static(path.join(__dirname)));
 
 cloudinary.config({ 
   cloud_name: process.env.CLOUD_NAME, 
@@ -125,6 +129,10 @@ error:error.message
 
 
 const PORT = 5000;
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "index.html"));
+});
 
 app.listen(PORT, () => {
 console.log("Server running on port " + PORT);
